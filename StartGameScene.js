1355d2d5986e49d9afdb4495f75867070d12da68
PoojaMittal2842 <72434596+PoojaMittal2842@users.noreply.github.com>
2020-10-15 12:30:24 +0530

    Update StartGameScene.js

--- StartGameScene.js
+++ StartGameScene.js
@@ -1,6 +1,6 @@
 class startgamescene extends Phaser.Scene {
     constructor() {
-        super('Playy')
+        super('Playy')    //super constructor is used to invoke immediate parent class method
     }
 
     preload() {
@@ -8,8 +8,8 @@ class startgamescene extends Phaser.Scene {
         this.load.image('start', 'play-now.png')
     }
 
-    create() {
-        this.add.image(400, 300, 'start-game').setScale(1.8)
+    create() {     
+        this.add.image(400, 300, 'start-game').setScale(1.8) // setscale is used to resize the image
         this.startbtn = this.add.image(400,300, 'start')
         this.startbtn.setInteractive();                       //setInteractive
         this.startbtn.on('pointerdown', this.startGame, this)
